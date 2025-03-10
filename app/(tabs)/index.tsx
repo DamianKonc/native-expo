0e78bd27038bab440a3e65049cb429c00aba370e
Damiankonc <damian.koncewicz@wp.pl>
2025-03-10 11:13:32 +0100

    handle platform differences

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import { useState, useRef } from "react";
+import { View, Platform } from "react-native";
 import styled from "styled-components/native";
 import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { captureRef } from "react-native-view-shot";
+import domtoimage from "dom-to-image";
 import ImageViewer from "@/components/ImageViewer/ImageViewer";
 import CustomButton from "@/components/Button/CustomButton";
 import IconButton from "@/components/IconButton/IconButton";
@@ -12,7 +14,6 @@ import EmojiPicker from "@/components/EmojiPicker/EmojiPicker";
 import { ImageSource } from "expo-image";
 import EmojiList from "@/components/EmojiList/EmojiList";
 import EmojiSticker from "@/components/EmojiSticker/EmojiSticker";
-import { View } from "react-native";
 
 const PlaceholderImage = require("../../assets/images/background-image.png");
 
@@ -60,18 +61,35 @@ export default function Index() {
   };
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
+    if (Platform.OS !== "web") {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
 
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert("Saved");
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert("Saved");
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement("a");
+        link.download = "sticker-smash.jpeg";
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
 
